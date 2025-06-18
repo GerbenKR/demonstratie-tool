e1a7f0b704fa686dd15cd93cce68d24838678a25
GerbenKr <gerbiebor@outlook.com>
2025-06-18 15:06:13 +0200

    fix: updated some styling

--- components/CopyValue.tsx
+++ components/CopyValue.tsx
@@ -16,7 +16,7 @@ export default function CopyValue({ title, value }: CopyValueProps) {
             await navigator.clipboard.writeText(value);
             setCopied(true);
             setAlreadyCopied(true);
-            setTimeout(() => setCopied(false), 3000);
+            setTimeout(() => setCopied(false), 1000);
         } catch (err) {
             console.error('Fout bij kopiëren:', err);
         }
@@ -25,13 +25,13 @@ export default function CopyValue({ title, value }: CopyValueProps) {
     return (
         <div className="grid grid-cols-4">
             <div
-                className={`col-span-1 p-2  text-sm ${
+                className={`col-span-1 p-2 text-sm  duration-100 ${
                     alreadyCopied ? 'bg-green-300' : 'bg-slate-100'
                 }`}>
                 {title}
             </div>
             <div
-                className={`col-span-3 p-2 font-mono flex justify-between items-center duration-200 text-sm ${
+                className={`col-span-3 p-2 font-mono flex justify-between items-center duration-100 text-sm ${
                     copied ? 'bg-green-500 text-white' : 'bg-slate-200'
                 }`}
                 onClick={handleCopy}>
